72ca05c6e5945753c07fbe7682f77292f3401841
jim huang <jim.javathunderbird@gmail.com>
2012-09-06 14:54:27 -0700

    more functionalities to file system module

--- src/Phastlight/Module/FileSystem/Main.php
+++ src/Phastlight/Module/FileSystem/Main.php
@@ -7,4 +7,19 @@ class Main extends \Phastlight\Module
   {
     uv_fs_rename($this->getEventLoop(), $old_path, $new_path, $callback);
   }
+
+  public function unlink($path, $callback)
+  {
+    uv_fs_unlink($this->getEventLoop(), $path, $callback);
+  }
+
+  public function lstat($path, $callback)
+  {
+    uv_fs_lstat($this->getEventLoop(), $path, $callback);
+  }
+
+  public function readDir($path, $callback)
+  {
+    uv_fs_readdir($this->getEventLoop(), $path, 0, $callback);
+  }
 }
