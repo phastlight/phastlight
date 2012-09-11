5feb4290d354be08a641329b20275df03497e872
Jim Huang <jim.javathunderbird@gmail.com>
2012-09-11 21:05:17 +0000

    more implementation on net socket write

--- src/Phastlight/Module/NET/Socket.php
+++ src/Phastlight/Module/NET/Socket.php
@@ -18,7 +18,11 @@ class Socket extends \Phastlight\EventEmitter
   public function write($data, $callback)
   {
     if($this->stream){
-      uv_write($this->stream, $data, $callback);
+      $self = $this;
+      uv_write($this->stream, $data, function($stream, $stat) use ($self){
+        $self->setStream($stream);
+        $callback();        
+      });
     }  
   }
 
