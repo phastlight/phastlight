94bda75310f75c2c85ce7ec8d342a0d1d85d6cdc
Jim Huang <jim.javathunderbird@gmail.com>
2012-09-11 18:04:53 +0000

    method signature change for net createConnection

--- src/Phastlight/Module/NET/Main.php
+++ src/Phastlight/Module/NET/Main.php
@@ -3,17 +3,17 @@ namespace Phastlight\Module\NET;
 
 class Main extends \Phastlight\Module
 {
-  public function createServer(/* $options, */$connectionListener)
+  public function createServer(/* $options, */$connectionListener = '')
   {
   
   } 
 
-  public function connect($options, $connectionListener)
+  public function connect($options, $connectionListener = '')
   {
   
   }
 
-  public function createTCPConnection($port, $host = '127.0.0.1', $callback = '')
+  public function createTCPConnection($options = array('host' => '127.0.0.1', 'port' => 1337), $connectionListener = '')
   {
   
   }
