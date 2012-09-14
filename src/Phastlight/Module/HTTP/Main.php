7ccd278a24a791cc7005e06b5f7c4223afebfad2
Jim Huang <jim.javathunderbird@gmail.com>
2012-09-14 04:55:28 +0000

    better structure for generating server variables, add HTTP_HOST var

--- src/Phastlight/Module/HTTP/Main.php
+++ src/Phastlight/Module/HTTP/Main.php
@@ -8,67 +8,43 @@ class Main extends \Phastlight\Module
   private $serverResponse;
   private $server;
   private $requestListener;
+  private $port;
+  private $host;
 
   public function createServer($requestListener)
   {
     $this->requestListener = $requestListener;
     $this->serverRequest = new ServerRequest();
     $this->serverResponse = new ServerResponse();
+    $this->http_parser = http_parser_init(); //create the http parser
     $server = clone($this); //tricky, we need to create a clone of the server object
     return $server;    
   } 
 
   public function listen($port, $host = '127.0.0.1', $backlog = 100)
   {
-    $_SERVER['SERVER_PORT'] = $port;
-    $_SERVER['SERVER_ADDR'] = $host;
+    $this->port = $port;
+    $this->host = $host;
 
     $self = $this;
 
     $system = $this->getSystem();
-    $process = $system->import("process");
 
     $this->server = uv_tcp_init();
 
-    $parser = http_parser_init();
     $request = $self->getServerRequest();
     $response = $self->getServerResponse();
     $requestListener = $self->getRequestListener();
 
     uv_tcp_bind($this->server, uv_ip4_addr($host, $port));
 
-    uv_listen($this->server,$backlog, function($server) use ($self, $parser, $process, &$request, &$response, &$requestListener) {
+    uv_listen($this->server,$backlog, function($server) use ($self, &$request, &$response, &$requestListener) {
       $client = uv_tcp_init();
       uv_accept($server, $client);
 
-      uv_read_start($client, function($socket, $nread, $buffer) use ($self, $parser, $process, &$request, &$response, &$requestListener){
-        $result = array();
+      uv_read_start($client, function($socket, $nread, $buffer) use ($self, &$request, &$response, &$requestListener){
 
-        $_SERVER['RAW_HTTP_HEADER'] = $buffer;
-
-        http_parser_execute($parser, $buffer, $result);
-
-        $requestMethod = $result['REQUEST_METHOD'];
-
-        //constructing server variables
-        $_SERVER['REQUEST_METHOD'] = $result['REQUEST_METHOD'];
-        if(isset($result['path'])){
-          $_SERVER['REQUEST_URI'] = $_SERVER['PATH_INFO'] = $result['path'];
-        }
-        if(isset($result['headers']['User-Agent'])){
-          $_SERVER['HTTP_USER_AGENT'] = $result['headers']['User-Agent'];
-        }
-
-        //constructing global variables
-        if($requestMethod == 'GET'){
-          if(isset($result['query'])){
-            $result['headers']['body'] = $result['query']; //bind body to query if it is a get request
-          }
-        }
-
-        if(isset($result['headers']['body'])){
-          $GLOBALS["_$requestMethod"] = explode("&", $result['headers']['body']); 
-        }
+        $self->generateServerVariables($buffer);
 
         call_user_func_array($requestListener, array($request, $response));
 
@@ -108,5 +84,43 @@ class Main extends \Phastlight\Module
   public function getProtocol()
   {
     return $this->protocol; 
+  }
+
+  public function generateServerVariables($buffer)
+  {
+    $_SERVER['SERVER_PORT'] = $this->port;
+    $_SERVER['SERVER_ADDR'] = $this->host;
+
+    $result = array();
+
+    $_SERVER['RAW_HTTP_HEADER'] = $buffer;
+
+    http_parser_execute($this->http_parser, $buffer, $result);
+
+    if(isset($result['headers']['Host'])){
+      $_SERVER['HTTP_HOST'] = $result['headers']['Host']; 
+    }
+
+    $requestMethod = $result['REQUEST_METHOD'];
+
+    //constructing server variables
+    $_SERVER['REQUEST_METHOD'] = $result['REQUEST_METHOD'];
+    if(isset($result['path'])){
+      $_SERVER['REQUEST_URI'] = $_SERVER['PATH_INFO'] = $result['path'];
+    }
+    if(isset($result['headers']['User-Agent'])){
+      $_SERVER['HTTP_USER_AGENT'] = $result['headers']['User-Agent'];
+    }
+
+    //constructing global variables
+    if($requestMethod == 'GET'){
+      if(isset($result['query'])){
+        $result['headers']['body'] = $result['query']; //bind body to query if it is a get request
+      }
+    }
+
+    if(isset($result['headers']['body'])){
+      $GLOBALS["_$requestMethod"] = explode("&", $result['headers']['body']); 
+    }
   }
 }
