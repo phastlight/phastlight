6ede2d315ec239f07fb7ff4fbcb9590b51d7c926
jimthunderbird <jim.javathunderbird@gmail.com>
2012-12-11 09:20:32 -0800

    code style improvement

--- examples/multiple_http_servers.php
+++ examples/multiple_http_servers.php
@@ -11,10 +11,10 @@ $http = $system->import("http");
 $ports = array(1337, 8000, 9000, 9001, 9002);
 $ip = '127.0.0.1'; //change this to your ip
 
-foreach($ports as $port){
-  $http->createServer(function($req, $res) use ($port){
-    $res->writeHead(200, array('Content-Type' => 'text/plain'));
-    $res->end("Server is running on port $port");
-  })->listen($port, $ip);
+foreach ($ports as $port) {
+    $http->createServer(function($req, $res) use ($port) {
+        $res->writeHead(200, array('Content-Type' => 'text/plain'));
+        $res->end("Server is running on port $port");
+    })->listen($port, $ip);
 }
 
