1707a2d33f8fefb29b85b43f4c49e71868ff4464
jimthunderbird <jim.javathunderbird@gmail.com>
2012-12-25 00:01:31 -0800

    use application/json in dbslayer example

--- examples/dbslayer.php
+++ examples/dbslayer.php
@@ -11,7 +11,7 @@ $client = $net->connect(array('host' => '127.0.0.1', 'port' => 9090), function()
         print_r($data);
     });
     $sql = urlencode('{"SQL":"SELECT VERSION();"}');
-    $msg = "GET /db?$sql HTTP/1.1\r\nHost: 127.0.0.1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:17.0) Gecko/20100101 Firefox/17.0\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8,application/json\r\nAccept-Language: en-US,en;q=0.5\r\n\r\n";
+    $msg = "GET /db?$sql HTTP/1.1\r\nHost: 127.0.0.1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:17.0) Gecko/20100101 Firefox/17.0\r\nAccept:application/json\r\nAccept-Language: en-US,en;q=0.5\r\n\r\n";
 
     $client->write($msg);
 });
