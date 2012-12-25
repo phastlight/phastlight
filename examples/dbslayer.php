92dfc0fbea8dfba92c4c93290f97eb92302fd02e
jimthunderbird <jim.javathunderbird@gmail.com>
2012-12-24 23:54:12 -0800

    add dbslayer example

---
+++ examples/dbslayer.php
@@ -0,0 +1,19 @@
+<?php
+//Assuming this is server/server.php and the composer vendor directory is ../vendor
+require_once __DIR__.'/../vendor/autoload.php';
+
+$system = new \Phastlight\System();
+
+$net = $system->import("net");
+
+$client = $net->connect(array('host' => '127.0.0.1', 'port' => 9090), function() use (&$client) {
+    $client->on('data', function($data) use (&$client) {
+        print_r($data);
+    });
+    $sql = urlencode('{"SQL":"SELECT VERSION();"}');
+    $msg = "GET /db?$sql HTTP/1.1\r\nHost: 127.0.0.1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:17.0) Gecko/20100101 Firefox/17.0\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8,application/json\r\nAccept-Language: en-US,en;q=0.5\r\n\r\n";
+
+    $client->write($msg);
+});
+
+
