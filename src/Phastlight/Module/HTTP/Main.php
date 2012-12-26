ba4f2944032f87ad893189dfe7eeb511f733151e
jimthunderbird <jim.javathunderbird@gmail.com>
2012-12-25 23:23:22 -0800

    fix process next tick, refactor http parser variable

--- src/Phastlight/Module/HTTP/Main.php
+++ src/Phastlight/Module/HTTP/Main.php
@@ -3,132 +3,132 @@ namespace Phastlight\Module\HTTP;
 
 class Main extends \Phastlight\Module
 {
-  private $http_parser;
-  private $serverRequest;
-  private $serverResponse;
-  private $server;
-  private $requestListener;
-  private $port;
-  private $host;
+    private $httpParser;
+    private $serverRequest;
+    private $serverResponse;
+    private $server;
+    private $requestListener;
+    private $port;
+    private $host;
 
-  public function __construct()
-  {
-    $this->http_parser = uv_http_parser_init(\UV::HTTP_REQUEST); //set up http parser
-  }
-
-  public function createServer($requestListener)
-  {
-    $this->requestListener = $requestListener;
-    $this->serverRequest = new ServerRequest();
-    $this->serverResponse = new ServerResponse();
-    $server = clone($this); //tricky, we need to create a clone of the server object
-    return $server;    
-  } 
-
-  public function listen($port, $host = '127.0.0.1', $backlog = 100)
-  {
-    $this->port = $port;
-    $this->host = $host;
-
-    $self = $this;
-
-    $system = $this->getSystem();
-
-    $this->server = uv_tcp_init();
-
-    $request = $self->getServerRequest();
-    $response = $self->getServerResponse();
-    $requestListener = $self->getRequestListener();
-
-    uv_tcp_bind($this->server, uv_ip4_addr($host, $port));
-
-    uv_listen($this->server,$backlog, function($server) use ($self, &$request, &$response, &$requestListener) {
-      $client = uv_tcp_init();
-      uv_accept($server, $client);
-
-      uv_read_start($client, function($socket, $nread, $buffer) use ($self, &$request, &$response, &$requestListener){
-
-        $self->generateServerVariables($buffer, $socket);
-
-        call_user_func_array($requestListener, array($request, $response));
-
-        $status_code = $response->getStatusCode();
-        $statusMessage = $response->getReasonPhrase();
-        $headers = $response->getHeaders();
-        $header = "";
-        if(count($headers) > 0){
-          foreach($headers as $key => $val){
-            $header .= $key.": ".$val."\r\n";
-          }
-        } 
-        $output = $response->getData();
-        $buffer = "HTTP/1.1 $status_code $statusMessage\r\n$header\r\n$output";
-        uv_write($socket, $buffer);
-        uv_close($socket);
-      });
-
-    });
-  }
-
-  public function getRequestListener()
-  {
-    return $this->requestListener; 
-  }
-
-  public function getServerRequest()
-  {
-    return $this->serverRequest; 
-  }
-
-  public function getServerResponse()
-  {
-    return $this->serverResponse; 
-  }
-
-  public function getProtocol()
-  {
-    return $this->protocol; 
-  }
-
-  public function generateServerVariables($buffer, $socket) {
-    $_SERVER['SERVER_PORT'] = $this->port;
-    $_SERVER['SERVER_ADDR'] = $this->host;
-
-    $socketInfo = uv_tcp_getpeername($socket);
-
-    if(isset($socketInfo['address'])){
-      $_SERVER['REMOTE_ADDR'] = $socketInfo['address']; 
+    public function __construct()
+    {
+        $this->httpParser = uv_http_parser_init(\UV::HTTP_REQUEST); //set up http parser
     }
 
-    $result = array();
+    public function createServer($requestListener)
+    {
+        $this->requestListener = $requestListener;
+        $this->serverRequest = new ServerRequest();
+        $this->serverResponse = new ServerResponse();
+        $server = clone($this); //tricky, we need to create a clone of the server object
+        return $server;    
+    } 
 
-    $_SERVER['RAW_HTTP_HEADER'] = $buffer;
+    public function listen($port, $host = '127.0.0.1', $backlog = 100)
+    {
+        $this->port = $port;
+        $this->host = $host;
 
-    uv_http_parser_execute($this->http_parser, $buffer, $result);
+        $self = $this;
 
-    if(isset($result['HEADERS']['HOST'])){
-      $_SERVER['HTTP_HOST'] = $result['HEADERS']['HOST']; 
+        $system = $this->getSystem();
+
+        $this->server = uv_tcp_init();
+
+        $request = $self->getServerRequest();
+        $response = $self->getServerResponse();
+        $requestListener = $self->getRequestListener();
+
+        uv_tcp_bind($this->server, uv_ip4_addr($host, $port));
+
+        uv_listen($this->server,$backlog, function($server) use ($self, &$request, &$response, &$requestListener) {
+            $client = uv_tcp_init();
+            uv_accept($server, $client);
+
+            uv_read_start($client, function($socket, $nread, $buffer) use ($self, &$request, &$response, &$requestListener) {
+
+                $self->generateServerVariables($buffer, $socket);
+
+                call_user_func_array($requestListener, array($request, $response));
+
+                $status_code = $response->getStatusCode();
+                $statusMessage = $response->getReasonPhrase();
+                $headers = $response->getHeaders();
+                $header = "";
+                if (count($headers) > 0) {
+                    foreach ($headers as $key => $val) {
+                        $header .= $key.": ".$val."\r\n";
+                    }
+                } 
+                $output = $response->getData();
+                $buffer = "HTTP/1.1 $status_code $statusMessage\r\n$header\r\n$output";
+                uv_write($socket, $buffer);
+                uv_close($socket);
+            });
+
+        });
     }
-    $requestMethod = $result['REQUEST_METHOD'];
 
-    //constructing server variables
-    $_SERVER['REQUEST_METHOD'] = $result['REQUEST_METHOD'];
-    if(isset($result['PATH'])){
-      $_SERVER['REQUEST_URI'] = $_SERVER['PATH_INFO'] = $result['PATH'];
-    }
-    if(isset($result['HEADERS']['USER_AGENT'])){
-      $_SERVER['HTTP_USER_AGENT'] = $result['HEADERS']['USER_AGENT'];
+    public function getRequestListener()
+    {
+        return $this->requestListener; 
     }
 
-    //constructing global variables
-    if($requestMethod == 'GET'){
-      if(isset($result['QUERY'])){
-        $result['HEADERS']['body'] = $result['QUERY']; //bind body to query if it is a get request
-      }
+    public function getServerRequest()
+    {
+        return $this->serverRequest; 
     }
 
-    if(isset($result['HEADERS']['body'])){
-      $GLOBALS["_$requestMethod"] = explode("&", $result['HEADERS']['body']); 
+    public function getServerResponse()
+    {
+        return $this->serverResponse; 
     }
-  }
+
+    public function getProtocol()
+    {
+        return $this->protocol; 
+    }
+
+    public function generateServerVariables($buffer, $socket) {
+        $_SERVER['SERVER_PORT'] = $this->port;
+        $_SERVER['SERVER_ADDR'] = $this->host;
+
+        $socketInfo = uv_tcp_getpeername($socket);
+
+        if (isset($socketInfo['address'])) {
+            $_SERVER['REMOTE_ADDR'] = $socketInfo['address']; 
+        }
+
+        $result = array();
+
+        $_SERVER['RAW_HTTP_HEADER'] = $buffer;
+
+        uv_http_parser_execute($this->httpParser, $buffer, $result);
+
+        if (isset($result['HEADERS']['HOST'])) {
+            $_SERVER['HTTP_HOST'] = $result['HEADERS']['HOST']; 
+        }
+        $requestMethod = $result['REQUEST_METHOD'];
+
+        //constructing server variables
+        $_SERVER['REQUEST_METHOD'] = $result['REQUEST_METHOD'];
+        if (isset($result['PATH'])) {
+            $_SERVER['REQUEST_URI'] = $_SERVER['PATH_INFO'] = $result['PATH'];
+        }
+        if (isset($result['HEADERS']['USER_AGENT'])) {
+            $_SERVER['HTTP_USER_AGENT'] = $result['HEADERS']['USER_AGENT'];
+        }
+
+        //constructing global variables
+        if ($requestMethod == 'GET') {
+            if (isset($result['QUERY'])) {
+                $result['HEADERS']['body'] = $result['QUERY']; //bind body to query if it is a get request
+            }
+        }
+
+        if (isset($result['HEADERS']['body'])) {
+            $GLOBALS["_$requestMethod"] = explode("&", $result['HEADERS']['body']); 
+        }
+    }
 }
--- src/Phastlight/Module/Process/Main.php
+++ src/Phastlight/Module/Process/Main.php
@@ -3,55 +3,56 @@ namespace Phastlight\Module\Process;
 
 class Main extends \Phastlight\Module
 {
-  private $tickCallbacks = array();
-  private $tick = 0;
+    private $tickCallbacks = array();
+    private $tick = 0;
 
-  public function addTickCallback($callback)
-  {
-    $this->tick ++;
-    $this->tickCallbacks[$this->tick] = $callback;
-  }
-
-  public function getTickCallback($tick)
-  {
-    return $this->tickCallbacks[$tick]; 
-  }
-
-  public function removeTickCallback($tick)
-  {
-    unset($this->tickCallbacks[$tick]); 
-  }
-
-  public function getTickCallbacks()
-  {
-    return $this->tickCallbacks; 
-  }
-
-  public function nextTick($callback)
-  {
-    $loop = $this->getSystem()->getEventLoop();
-
-    $plugin = $this;
-    $plugin->addTickCallback($callback);
-
-    $tick = $this->tick;
-
-    if($tick == 1){
-      $f = function($r, $status) use ($plugin, &$tick){
-        $callback = $plugin->getTickCallback($tick);
-        if(is_callable($callback)){
-          $callback();
-          $plugin->removeTickCallback($tick);
-          $tick ++;
-          uv_async_send($r);
-        }
-        else{
-          uv_close($r); 
-        }
-      };
-
-      $r = uv_async_init($loop, $f);
-      uv_async_send($r);
+    public function addTickCallback($callback)
+    {
+        $this->tick ++;
+        $this->tickCallbacks[$this->tick] = $callback;
     }
-  }
+
+    public function getTickCallback($tick)
+    {
+        return $this->tickCallbacks[$tick]; 
+    }
+
+    public function removeTickCallback($tick)
+    {
+        unset($this->tickCallbacks[$tick]); 
+    }
+
+    public function getTickCallbacks()
+    {
+        return $this->tickCallbacks; 
+    }
+
+    public function nextTick($callback)
+    {
+        $loop = $this->getSystem()->getEventLoop();
+
+        $plugin = $this;
+        $plugin->addTickCallback($callback);
+
+        $tick = $this->tick;
+
+        if ($tick == 1) {
+            $f = function($r, $status) use ($plugin, &$tick) {
+                $callback = $plugin->getTickCallback($tick);
+                if (is_callable($callback)) {
+                    //$callback();
+                    call_user_func($callback);
+                    $plugin->removeTickCallback($tick);
+                    $tick ++;
+                    uv_async_send($r);
+                }
+                else{
+                    uv_close($r); 
+                }
+            };
+
+            $r = uv_async_init($loop, $f);
+            uv_async_send($r);
+        }
+    }
 }
