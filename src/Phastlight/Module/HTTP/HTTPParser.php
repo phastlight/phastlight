c1bb6281ba4d9a7958836167d1956d480ef2f1b0
jim zike huang <jim.huang@gree.net>
2014-02-21 20:36:52 -0800

    spacing fix

--- src/Phastlight/Module/HTTP/HTTPParser.php
+++ src/Phastlight/Module/HTTP/HTTPParser.php
@@ -3,11 +3,11 @@ namespace Phastlight\Module\HTTP;
 
 class HTTPParser
 {
-    public function parse($content)
-    {
-        $result = array();
-        $parser = uv_http_parser_init(\UV::HTTP_REQUEST); //set up http parser
-        uv_http_parser_execute($parser, $content, $result);
-        return $result;
-    }
+  public function parse($content)
+  {
+    $result = array();
+    $parser = uv_http_parser_init(\UV::HTTP_REQUEST); //set up http parser
+    uv_http_parser_execute($parser, $content, $result);
+    return $result;
+  }
 } 
--- src/Phastlight/Module/HTTP/Main.php
+++ src/Phastlight/Module/HTTP/Main.php
@@ -3,125 +3,125 @@ namespace Phastlight\Module\HTTP;
 
 class Main extends \Phastlight\Module
 {
-    private $httpParser;
-    private $serverRequest;
-    private $serverResponse;
-    private $server;
-    private $requestListener;
-    private $port;
-    private $host;
+  private $httpParser;
+  private $serverRequest;
+  private $serverResponse;
+  private $server;
+  private $requestListener;
+  private $port;
+  private $host;
 
-    public function __construct()
-    {
-        $this->httpParser = new HTTPParser();
+  public function __construct()
+  {
+    $this->httpParser = new HTTPParser();
+  }
+
+  public function createServer($requestListener)
+  {
+    $this->requestListener = $requestListener;
+    $this->serverRequest = new ServerRequest();
+    $this->serverResponse = new ServerResponse();
+    $server = clone($this); //tricky, we need to create a clone of the server object
+    return $server;    
+  } 
+
+  public function listen($port, $host = '127.0.0.1', $backlog = 100)
+  {
+    $this->port = $port;
+    $this->host = $host;
+
+    $self = $this;
+
+    $system = $this->getSystem();
+
+    $this->server = uv_tcp_init();
+
+    $request = $self->getServerRequest();
+    $response = $self->getServerResponse();
+    $requestListener = $self->getRequestListener();
+
+    uv_tcp_bind($this->server, uv_ip4_addr($host, $port));
+
+    uv_listen($this->server,$backlog, function($server) use ($self, &$request, &$response, &$requestListener) {
+      $client = uv_tcp_init();
+      uv_accept($server, $client);
+
+      uv_read_start($client, function($socket, $nread, $buffer) use ($self, &$request, &$response, &$requestListener) {
+
+        $self->generateServerVariables($buffer, $socket);
+
+        call_user_func_array($requestListener, array($request, $response));
+
+        $status_code = $response->getStatusCode();
+        $statusMessage = $response->getReasonPhrase();
+        $headers = $response->getHeaders();
+        $header = "";
+        if (count($headers) > 0) {
+          foreach ($headers as $key => $val) {
+            $header .= $key.": ".$val."\r\n";
+          }
+        } 
+        $output = $response->getData();
+        $buffer = "HTTP/1.1 $status_code $statusMessage\r\n$header\r\n$output";
+        uv_write($socket, $buffer);
+        uv_close($socket);
+      });
+
+    });
+  }
+
+  public function getRequestListener()
+  {
+    return $this->requestListener; 
+  }
+
+  public function getServerRequest()
+  {
+    return $this->serverRequest; 
+  }
+
+  public function getServerResponse()
+  {
+    return $this->serverResponse; 
+  }
+
+  public function getProtocol()
+  {
+    return $this->protocol; 
+  }
+
+  public function generateServerVariables($buffer, $socket) {
+    $_SERVER['SERVER_PORT'] = $this->port;
+    $_SERVER['SERVER_ADDR'] = $this->host;
+
+    $socketInfo = uv_tcp_getpeername($socket);
+
+    if (isset($socketInfo['address'])) {
+      $_SERVER['REMOTE_ADDR'] = $socketInfo['address']; 
     }
 
-    public function createServer($requestListener)
-    {
-        $this->requestListener = $requestListener;
-        $this->serverRequest = new ServerRequest();
-        $this->serverResponse = new ServerResponse();
-        $server = clone($this); //tricky, we need to create a clone of the server object
-        return $server;    
-    } 
+    $result = array();
 
-    public function listen($port, $host = '127.0.0.1', $backlog = 100)
-    {
-        $this->port = $port;
-        $this->host = $host;
+    $_SERVER['RAW_HTTP_HEADER'] = $buffer;
 
-        $self = $this;
-
-        $system = $this->getSystem();
-
-        $this->server = uv_tcp_init();
-
-        $request = $self->getServerRequest();
-        $response = $self->getServerResponse();
-        $requestListener = $self->getRequestListener();
-
-        uv_tcp_bind($this->server, uv_ip4_addr($host, $port));
-
-        uv_listen($this->server,$backlog, function($server) use ($self, &$request, &$response, &$requestListener) {
-            $client = uv_tcp_init();
-            uv_accept($server, $client);
-
-            uv_read_start($client, function($socket, $nread, $buffer) use ($self, &$request, &$response, &$requestListener) {
-
-                $self->generateServerVariables($buffer, $socket);
-
-                call_user_func_array($requestListener, array($request, $response));
-
-                $status_code = $response->getStatusCode();
-                $statusMessage = $response->getReasonPhrase();
-                $headers = $response->getHeaders();
-                $header = "";
-                if (count($headers) > 0) {
-                    foreach ($headers as $key => $val) {
-                        $header .= $key.": ".$val."\r\n";
-                    }
-                } 
-                $output = $response->getData();
-                $buffer = "HTTP/1.1 $status_code $statusMessage\r\n$header\r\n$output";
-                uv_write($socket, $buffer);
-                uv_close($socket);
-            });
-
-        });
+    $result = $this->httpParser->parse($buffer);
+    if (isset($result['HEADERS']['HOST'])) {
+      $_SERVER['HTTP_HOST'] = $result['HEADERS']['HOST']; 
     }
+    $requestMethod = $result['REQUEST_METHOD'];
 
-    public function getRequestListener()
-    {
-        return $this->requestListener; 
+    //constructing server variables
+    $_SERVER['REQUEST_METHOD'] = $result['REQUEST_METHOD'];
+    if (isset($result['PATH'])) {
+      $_SERVER['REQUEST_URI'] = $_SERVER['PATH_INFO'] = $result['PATH'];
     }
-
-    public function getServerRequest()
-    {
-        return $this->serverRequest; 
+    if (isset($result['HEADERS']['USER_AGENT'])) {
+      $_SERVER['HTTP_USER_AGENT'] = $result['HEADERS']['USER_AGENT'];
     }
+  }
 
-    public function getServerResponse()
-    {
-        return $this->serverResponse; 
-    }
-
-    public function getProtocol()
-    {
-        return $this->protocol; 
-    }
-
-    public function generateServerVariables($buffer, $socket) {
-        $_SERVER['SERVER_PORT'] = $this->port;
-        $_SERVER['SERVER_ADDR'] = $this->host;
-
-        $socketInfo = uv_tcp_getpeername($socket);
-
-        if (isset($socketInfo['address'])) {
-            $_SERVER['REMOTE_ADDR'] = $socketInfo['address']; 
-        }
-
-        $result = array();
-
-        $_SERVER['RAW_HTTP_HEADER'] = $buffer;
-
-        $result = $this->httpParser->parse($buffer);
-        if (isset($result['HEADERS']['HOST'])) {
-            $_SERVER['HTTP_HOST'] = $result['HEADERS']['HOST']; 
-        }
-        $requestMethod = $result['REQUEST_METHOD'];
-
-        //constructing server variables
-        $_SERVER['REQUEST_METHOD'] = $result['REQUEST_METHOD'];
-        if (isset($result['PATH'])) {
-            $_SERVER['REQUEST_URI'] = $_SERVER['PATH_INFO'] = $result['PATH'];
-        }
-        if (isset($result['HEADERS']['USER_AGENT'])) {
-            $_SERVER['HTTP_USER_AGENT'] = $result['HEADERS']['USER_AGENT'];
-        }
-    }
-
-    public function getParser()
-    {
-        return $this->httpParser;
-    }
+  public function getParser()
+  {
+    return $this->httpParser;
+  }
 }
