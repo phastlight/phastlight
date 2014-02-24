f4665c8325d7cd4de44018da11490ad31dc795d3
jim zike huang <jim.huang@gree.net>
2014-02-23 23:39:26 -0800

    update talk to redis example

--- examples/talk_to_redis.php
+++ examples/talk_to_redis.php
@@ -12,17 +12,11 @@ $client->on('connect',  function() use (&$client) {
     print "connected to redis server, sending key value over...\n";
     $crlf = "\r\n";
     $client->write("SET mykey myvalue234$crlf"); //we can set the value of a key in one simple command over tcp
+    $client->write("GET mykey$crlf"); //we can set the value of a key in one simple command over tcp
 
 });
 
 $client->on('data', function($data) use (&$client) {
-    $crlf = "\r\n";
-    if($data == "+OK$crlf"){ //from the protocol, we know that now the key is successfully stored
-        $client->removeAllListeners('data'); 
-        $client->on('data', function($data) use(&$client){
-            print $data; //here we can see the details of the key that we just stored  
-            $client->end(); //close the connection
-        });
-        $client->write("GET mykey$crlf"); //we can get the key in one simple command over tcp
-    }
+    print_r($data);
+    $client->end();
 });
