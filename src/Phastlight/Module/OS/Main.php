8cb4b10517d711b21456aa1c80268e5ee269976e
jim zike huang <jim.huang@gree.net>
2014-02-23 16:00:34 -0800

    ad get uptime

--- src/Phastlight/Module/OS/Main.php
+++ src/Phastlight/Module/OS/Main.php
@@ -3,23 +3,28 @@ namespace Phastlight\Module\OS;
 
 class Main extends \Phastlight\Module
 {
-  public function getEOL()
-  {
-    return PHP_EOL; 
-  }
+    public function getEOL()
+    {
+        return PHP_EOL; 
+    }
 
-  public function getCPUInfo()
-  {
-    return uv_cpu_info(); 
-  }
+    public function getCPUInfo()
+    {
+        return uv_cpu_info(); 
+    }
 
-  public function getFreeMemoryInfo()
-  {
-    return uv_get_free_memory();  
-  }
+    public function getFreeMemoryInfo()
+    {
+        return uv_get_free_memory();  
+    }
 
-  public function getTotalMemoryInfo()
-  {
-    return uv_get_total_memory(); 
-  }
+    public function getTotalMemoryInfo()
+    {
+        return uv_get_total_memory(); 
+    }
+
+    public function getUptime() 
+    {
+        return uv_uptime();
+    }
 }
