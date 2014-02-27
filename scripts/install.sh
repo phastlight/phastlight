3521b0799e125960bd8810fa0062a0c8a603bfb3
jim zike huang <jim.huang@gree.net>
2014-02-27 10:01:09 -0800

    fix path with eval

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,33 +1,42 @@
 echo "Starting phastlight installation..."
 
-dir=/usr/local/phastlight
-phpversion="5.5.9"
+# where will the phastlight be installed to?
+phastlight_dir=/usr/local/phastlight 
+# the php version that is tied to phastlight 
+phpversion="5.5.9" 
+# the path of the phastlight executable
+phastlight_executable_path=/usr/local/bin
 
 for i in "$@"
 do 
     case $i in 
-        --install-dir=*)
-            dir="${i#*=}"
+        --phastlight-dir=*)
+            comps=(${i//=/ })
+            phastlight_dir=${comps[1]}
             ;;
         --phpversion=*)
-            phpversion="${i#*=}"
+            comps=(${i//=/ })
+            phpversion=${comps[1]}
             ;;
-
+        --phastlight_executable_path=*)
+            comps=(${i//=/ })
+            phastlight_executable_path=${comps[1]}
+            ;;
     esac
 done 
 
-if [ ! -d $dir ]; 
-then
-    mkdir $dir
-fi
-cd $dir
+phastlight_dir=$(echo eval $phastlight_dir)
+phastlight_executable_path=$(echo eval $phastlight_executable_path)
+
+sudo mkdir -p $phastlight_dir
+sudo mkdir -p $phastlight_executable_path 
 
 echo "Installing php..."
 # Download php src and enable sockets extension
 wget http://us2.php.net/get/php-$phpversion.tar.gz/from/this/mirror -O php-$phpversion.tar.gz
 tar xvf php-$phpversion.tar.gz
 cd php-$phpversion
-sudo ./configure --enable-sockets --prefix=$dir 
+sudo ./configure --enable-sockets --prefix=$phastlight_dir 
 sudo make 
 sudo make install
 cd ..
@@ -35,7 +44,7 @@ cd ..
 # get the current $PATH 
 OLDPATH=$PATH 
 # override path so we can switch to phastlight's php 
-export PATH=$dir/bin:$PATH
+export PATH=$phastlight_dir/bin:$PATH
 
 echo "Installing php-uv..."
 # Install php-uv
@@ -47,14 +56,14 @@ sudo phpize
 sudo make
 sudo make install
 
-cd $dir
+cd $phastlight_dir
 
 # resume old path 
 export PATH=$OLDPATH
 
 # write php.ini file
 
-extension_dir=$($dir/bin/php-config --extension-dir)
+extension_dir=$($phastlight_dir/bin/php-config --extension-dir)
 
 echo "extension_dir=$extension_dir" > php.ini 
 nl=$'\n'
@@ -63,18 +72,18 @@ echo "extension=uv.so" >> php.ini
 
 # generate phastlight executable  
 echo "Generating phastlight binary at /usr/local/bin"
-sudo echo "$dir/bin/php -c $dir/php.ini \$*" > $dir/bin/phastlight 
-sudo chmod u+x $dir/bin/phastlight 
-sudo rm -f /usr/local/bin/phastlight
-sudo ln -s $dir/bin/phastlight /usr/local/bin/phastlight
+sudo echo "$phastlight_dir/bin/php -c $phastlight_dir/php.ini \$*" > $phastlight_dir/bin/phastlight 
+sudo chmod u+x $phastlight_dir/bin/phastlight 
+sudo rm -f $phastlight_executable_path/phastlight
+sudo ln -s $phastlight_dir/bin/phastlight $phastlight_executable_path/phastlight
 
 # installing composer 
 echo "Installing composer...\n"
-curl -sS https://getcomposer.org/installer | $dir/bin/php -- --install-dir=$dir/bin
+curl -sS https://getcomposer.org/installer | $phastlight_dir/bin/php -- --install-dir=$phastlight_dir/bin
 
-if [ $($dir/bin/phastlight -m | grep uv | wc -l) -eq 1 ]; then
+if [ $($phastlight_executable_path/phastlight -m | grep uv | wc -l) -eq 1 ]; then
     echo "Installation completed, you can start phastlight with"
     echo "phastlight [Your sever file]"
 else
-    echo "Fail installing uv.so extension to $dir/bin/php"
-fi
+    echo "Fail installing uv.so extension to $phastlight_dir/bin/php"
+ffi
