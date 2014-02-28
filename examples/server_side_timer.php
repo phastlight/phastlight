701a3af93c91ae33a7d151ceaed0b2643e1bfbc0
jim zike huang <jim.huang@gree.net>
2014-02-27 23:17:44 -0800

    add server side timer example

---
+++ examples/server_side_timer.php
@@ -0,0 +1,14 @@
+<?php
+$system = new \Phastlight\System();
+
+$timer = $system->import("timer");
+$count = 0;
+$intervalId = $timer->setInterval(function($word) use (&$count, &$intervalId, $timer){
+  $count ++;
+  if($count <=3){
+    echo $count.":".$word."\n";
+  }
+  else{
+    $timer->clearInterval($intervalId); 
+  }
+}, 1000, "world");
