0b2f0ac52295c7ff970d39485b29ac37252061aa
jim zike huang <jim.huang@gree.net>
2014-03-01 20:09:00 -0800

    update example for singleton

--- examples/system_singleton.php
+++ examples/system_singleton.php
@@ -4,3 +4,14 @@ function p($name) {
 }
 
 p('console')->log("from system singleton");
+
+$count = 0;
+$intervalId = p('timer')->setInterval(function($word) use (&$count, &$intervalId){
+    $count ++;
+    if($count <=3){
+        echo $count.":".$word."\n";
+    }
+    else{
+        p('timer')->clearInterval($intervalId); 
+    }
+}, 1000, "world");
