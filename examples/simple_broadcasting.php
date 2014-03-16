04e67e4c30d3f1ab5bde071aa68750e9448d030f
jim zike huang <jim.huang@gree.net>
2014-03-15 23:40:12 -0700

    fix broadcasting example

--- examples/simple_broadcasting.php
+++ examples/simple_broadcasting.php
@@ -8,18 +8,20 @@ $http = $system->import("http");
 $ip = '127.0.0.1'; //change this to your ip
 $port = 1337;
 
-$http->createServer(function($req, $res) use ($port, &$system) {
+$numOfConnections = 0;
+$http->createServer(function($req, $res) use ($port, &$system, &$numOfConnections) {
+    $numOfConnections ++;
     //// Broadcasting every 2 seconds
     $timer = $system->import("timer");
     $count = 0;
-    $intervalId = $timer->setInterval(function($word) use (&$count, &$intervalId, &$timer, &$res){
+    $intervalId = $timer->setInterval(function() use (&$count, &$intervalId, &$timer, &$res, &$numOfConnections){
         $count ++;
         if($count <=100){
-            $res->write("hello ".$word);
+            $res->write("numOfConnections: $numOfConnections");
         }
         else{
             $timer->clearInterval($intervalId); 
         }
-    }, 2000, "world");
+    }, 2000);
 })->listen($port, $ip);
 $console->log("Server is running on port $port");
